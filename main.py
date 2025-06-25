40816ee4b19b1f195b05f8349032ff363d2224d8
GermanJavierR <german.j.r1002@gmail.com>
2025-06-25 15:28:29 -0300

    cambie el nombre de la función que ejecuta el juego y el nombre de la variable running por jugando como lo pide en el punto 10

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ import pygame
 import pygame.mixer as mixer # importo pygame.mixer como mixer para poder usar sonidos más adelante.
 from texto import mostrar_texto
 
-def main():
-    pygame.init() # Inicializando pygame
-    ancho, alto = 800, 600 # declarando el ancho y el alto que tenda la pantalla.
+pygame.init() # Inicializando pygame
+ancho, alto = 800, 600 # declarando el ancho y el alto que tenda la pantalla.
 
 
-    screen = pygame.display.set_mode((ancho, alto), pygame.SCALED) # Se crea la pantalla y se la asigna a screen. A esta se le pasan los valores de alto y ancho que definimos anteriormente. También se usa pygame.SCALED.
-    clock = pygame.time.Clock() # 
-    running = True # Se inicializa la varianble running con True para que inicie el juego.
+screen = pygame.display.set_mode((ancho, alto), pygame.SCALED) # Se crea la pantalla y se la asigna a screen. A esta se le pasan los valores de alto y ancho que definimos anteriormente. También se usa pygame.SCALED.
+clock = pygame.time.Clock() # 
 
-    while running: # Se crea el bucle principal del juego.
+
+def jugar():
+  
+    jugando = True # Se inicializa la varianble jugando con True para que inicie el juego.
+
+    while jugando: # Se crea el bucle principal del juego.
         events = pygame.event.get() # Se obtienen todos los eventos que ocurren con .get() y se guardan en events.
         for event in events: # Para cada event en events se verifica lo siguiente.
             if event.type == pygame.QUIT: # Si el tipo del evento actual es igual a pygame.QUIT entonces
-                running = False # Se le asigna False a la variable running
+                jugando = False # Se le asigna False a la variable running
 
         # Actualizar el juego (por ahora nada)
 
@@ -26,4 +29,4 @@ def main():
 
 
 
-main()
+jugar()
